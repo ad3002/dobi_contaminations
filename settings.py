f4cfcfd5f1c520a9ab284f6287eacd6a133a515c
Aleksey Komissarov <ad3002@gmail.com>
2015-03-22 10:34:27 +0100

    local settings

---
+++ settings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 16.03.2015
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com
+
+settings = {
+	"folder_tsa": "/mnt/guatemala/akomissarov/ncbi/tsa",
+	"folder_wgs": "/mnt/gonduras/akomissarov/wgs/wgs",
+	"folder_genomes_eu": "/mnt/guatemala/akomissarov/ncbi/genomes/Eukaryotes",
+	"folder_genomes_pro": "/mnt/guatemala/akomissarov/ncbi/genomes/Bacteria",
+}
