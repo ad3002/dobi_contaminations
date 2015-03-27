2f2e946a42f442f349a5a7700223a7076ef0178d
Aleksey Komissarov <ad3002@gmail.com>
2015-03-27 16:59:37 +0400

    path to kmers

--- settings.py
+++ settings.py
@@ -11,4 +11,5 @@ settings = {
 	"folder_genomes_eu": "/mnt/guatemala/akomissarov/ncbi/genomes/Eukaryotes",
 	"folder_genomes_pro": "/mnt/guatemala/akomissarov/ncbi/genomes/Bacteria",
 	"jf_path": "/home/akomissarov/libs/jellyfish-2.2.0/bin/jellyfish",
+	"kmers_illumina": "/home/akomissarov/Dropbox/PyBioSnippets/hiseq/illumina.23.dat",
 }
